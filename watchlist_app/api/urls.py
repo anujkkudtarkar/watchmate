2912509dfb351b67ac6b0b6a916f1b807c216ed2
DS <ds@DSs-MacBook-Air.local>
2021-12-18 21:50:15 +0530

    added generic class based view

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 
-from watchlist_app.api.views import WatchlistAV, WatchDetailAV
+from watchlist_app.api.views import WatchlistAV, WatchDetailAV, StreamPlatformListAV, StreamPlatformDetailAV, ReviewlistAV, ReviewDetailAV
 
 urlpatterns = [
     path('list/', WatchlistAV.as_view(), name='movie-list'),
-    path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),    
+    path('<int:pk>', WatchDetailAV.as_view(), name='movie-details'),
+    path('stream/', StreamPlatformListAV.as_view(), name='streamplatform-list'),
+    path('stream/<int:pk>', StreamPlatformDetailAV.as_view(), name='streamplatform-details'),
+    path('review/', ReviewlistAV.as_view(), name='review-list'),
+    path('review/<int:pk>', ReviewDetailAV.as_view(), name='review-details'),                  
 ]
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -1,42 +1,67 @@
 from django.shortcuts import render
-from watchlist_app.models import WatchList, StreamPlatform
+from watchlist_app.models import WatchList, StreamPlatform, Review
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from watchlist_app.api.serializers import StreamPlatformSerializer, WatchListSerializer
+from watchlist_app.api.serializers import StreamPlatformSerializer, WatchListSerializer, ReviewSerializer
+from rest_framework import generics
 
 # Create your views here.
 
-class WatchlistAV(APIView):
+class WatchlistAV(generics.ListCreateAPIView):
+    queryset = WatchList.objects.all()
+    serializer_class = WatchListSerializer
     
-    def get(self,request, *args, **kwargs):
-        movies = WatchList.objects.all()
-        serializer = WatchListSerializer(movies, many=True)
-        return Response(serializer.data)
+class WatchDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = WatchList.objects.all()
+    serializer_class = WatchListSerializer    
     
-    def post(self,request, *args, **kwargs):
-        serializer = WatchListSerializer(data= request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+class StreamPlatformListAV(generics.ListCreateAPIView):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
+    
+class StreamPlatformDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
+    
+class ReviewlistAV(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    
+class ReviewDetailAV(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer        
+
+# class WatchlistAV(APIView):
+    
+#     def get(self,request, *args, **kwargs):
+#         movies = WatchList.objects.all()
+#         serializer = WatchListSerializer(movies, many=True)
+#         return Response(serializer.data)
+    
+#     def post(self,request, *args, **kwargs):
+#         serializer = WatchListSerializer(data= request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else:
+#             return Response(serializer.errors)
         
-class WatchDetailAV(APIView):
+# class WatchDetailAV(APIView):
     
-    def get(self,request, pk, *args, **kwargs):
-        movie = WatchList.objects.get(pk=pk)
-        serializer = WatchListSerializer(movie)
-        return Response(serializer.data)
+#     def get(self,request, pk, *args, **kwargs):
+#         movie = WatchList.objects.get(pk=pk)
+#         serializer = WatchListSerializer(movie)
+#         return Response(serializer.data)
     
-    def put(self,request,pk, *args, **kwargs):
-        movie = WatchList.objects.get(pk=pk)
-        serializer = WatchListSerializer(movie, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else: 
-            return Response(serializer.errors)
+#     def put(self,request,pk, *args, **kwargs):
+#         movie = WatchList.objects.get(pk=pk)
+#         serializer = WatchListSerializer(movie, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         else: 
+#             return Response(serializer.errors)
         
     # def patch(self,request,pk, *args, **kwargs):
     #     movie = WatchList.objects.get(pk=pk)
